9ff741727a45200b706933a2449ec5d0425836f6
Khanh <nhatkhanh2v2@gmail.com>
2024-10-11 20:17:31 +0700

    nop 1.1 den 1.9

--- baitapchuong3/ex3chuong3.java
+++
@@ -1,8 +0,0 @@
-package baitapchuong3;
-public class ex3chuong3{
-    public static void main(String[] args){
-        for(int i = 1; i <= 100; i = i+1){
-            System.out.println(i);
-        }
-    }
-}
